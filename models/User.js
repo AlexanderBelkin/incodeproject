ba7ea11490497de3426dcde125f006de5d20314c
Bartieniev Dmytro <kuhelbeher@gmail.com>
2018-07-23 12:08:59 +0300

    Finished users route (registration and login)

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+// Create User Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const User = mongoose.model('users', UserSchema);
+
+module.exports = User;
